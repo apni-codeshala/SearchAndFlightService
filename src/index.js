bb1151036fb4af345389fc323a7311bae1eb8939
Abhishek Patel <abhishelpatel171019@gmail.com>
2023-09-07 01:21:17 +0530

    Added routers to complete CRUD operation

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const { PORT } = require('./config/serverConfig');
+const  ApiRoutes = require('./routes/index')
 
 const setupAndStartServer = async () => {
 
@@ -11,6 +12,8 @@ const setupAndStartServer = async () => {
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
     
+    app.use('/api', ApiRoutes);
+
     app.listen(PORT, ()=>{
         console.log(`Server startes at ${PORT}`);
     });
---
+++ src/routes/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+
+const v1ApRoutes = require('./v1/index');
+
+router.use('/v1', v1ApRoutes);
+module.exports = router;
