720b59c591ff046ad26061377d94ff264658567d
Abhishek Patel <abhishelpatel171019@gmail.com>
2023-09-10 20:43:25 +0530

    Added airplanes

--- src/seeders/20230909141225-add-airports.js
+++ src/seeders/20230909141225-add-airports.js
@@ -13,43 +13,43 @@ module.exports = {
      * }], {});
     */
 
-    await queryInterface.bulkInsert('Airports', [
-      {
-         name: "Surat International Airport",
-         address: "Surat, Gujarat",
-         cityId: 23,
-         createdAt: new Date(),
-         updatedAt: new Date()
-      },
-      {
-         name: "Daman Airport",
-         address: "Domestic Airport, Daman",
-         cityId: 24, 
-         createdAt: new Date(),
-         updatedAt: new Date()
-      },
-      {
-         name: "Nashik International Airport",
-         address: "Ojhar, Maharashtra",
-         cityId: 25,
-         createdAt: new Date(),
-         updatedAt: new Date()
-      },
-      {
-         name: "Rajiv Gandhi International Airport",
-         address: "Hyderabad, Telangana",
-         cityId: 5,
-         createdAt: new Date(),
-         updatedAt: new Date()
-      },
-      {
-         name: "Mumbai Airport",
-         address: "Mumbai, Maharashtra",
-         cityId: 8,
-         createdAt: new Date(),
-         updatedAt: new Date()
-      },
-    ], {});
+   //  await queryInterface.bulkInsert('Airports', [
+   //    {
+   //       name: "Surat International Airport",
+   //       address: "Surat, Gujarat",
+   //       cityId: 23,
+   //       createdAt: new Date(),
+   //       updatedAt: new Date()
+   //    },
+   //    {
+   //       name: "Daman Airport",
+   //       address: "Domestic Airport, Daman",
+   //       cityId: 24, 
+   //       createdAt: new Date(),
+   //       updatedAt: new Date()
+   //    },
+   //    {
+   //       name: "Nashik International Airport",
+   //       address: "Ojhar, Maharashtra",
+   //       cityId: 25,
+   //       createdAt: new Date(),
+   //       updatedAt: new Date()
+   //    },
+   //    {
+   //       name: "Rajiv Gandhi International Airport",
+   //       address: "Hyderabad, Telangana",
+   //       cityId: 5,
+   //       createdAt: new Date(),
+   //       updatedAt: new Date()
+   //    },
+   //    {
+   //       name: "Mumbai Airport",
+   //       address: "Mumbai, Maharashtra",
+   //       cityId: 8,
+   //       createdAt: new Date(),
+   //       updatedAt: new Date()
+   //    },
+   //  ], {});
 
 
   },
